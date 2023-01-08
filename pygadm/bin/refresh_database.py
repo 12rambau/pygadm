e1c19f55d38280bbe6f4e2c32a47cfc5efc341c2
12rambau <pierrick.rambaud49@gmail.com>
2023-01-08 20:04:38 +0100

    refactor: use brotli compression for database

--- pygadm/bin/refresh_database.py
+++ pygadm/bin/refresh_database.py
@@ -78,4 +78,4 @@ if __name__ == "__main__":
     filename = Path(__file__).parents[1] / "data" / "gadm_database.parquet"
 
     # specifying the protocol for compatibility with Python 3.7
-    gadm_df_filtered.to_parquet(filename)
+    gadm_df_filtered.to_parquet(filename, compression="Brotli")
